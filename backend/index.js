0f82cacd0a2f93e66e42b70380c74a8fab3e17aa
Amrita Gupta <amamrita0818@gmail.com>
2023-12-06 03:58:20 +0530

    initial commit frontend

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8000
-const cors = require('cors')
-
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require("cors");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //password: Y2tgjpwjLVyzRMGW
 
 //mongoDB config
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bookish-mern-book-store:Y2tgjpwjLVyzRMGW@cluster0.cgyjafl.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://bookish-mern-book-store:Y2tgjpwjLVyzRMGW@cluster0.cgyjafl.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -37,26 +37,73 @@ async function run() {
     const bookCollections = client.db("BookInventory").collection("books");
 
     //insert a book to the db: post method
-    app.post("/upload-book", async(req, res) => {
+    app.post("/upload-book", async (req, res) => {
       const data = req.body;
       const result = await bookCollections.insertOne(data);
       res.send(result);
+    });
 
+    //get all books from the database
+    // app.get("/all-books", async (req, res) => {
+    //   const books = bookCollections.find();
+    //   const result = await books.toArray();
+    //   res.send(result);
+    // });
+
+    //update a book data: patch or update methods
+    app.patch("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+
+      const updateBookData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upset: true };
+
+      const updateDoc = {
+        $set: {
+          ...updateBookData,
+        },
+      };
+
+      //update
+      const result = await bookCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //delete a book data
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    //find by category
+    app.get("/all-books", async(req, res) => {
+      let query = {};
+      if(req.query?.category){
+        query = {category: req.query.category}
+      }
+      const result = await bookCollections.find(query).toArray();
+      res.send(result);
     })
 
-    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch(error){
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
     console.log(error);
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <nav>Site logo</nav>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
+  
