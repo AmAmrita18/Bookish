2a31dd473e13abc93eff0f6b6ddf61d7a55d6c59
Amrita Gupta <amamrita0818@gmail.com>
2023-12-06 17:07:22 +0530

    next commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -27,23 +28,47 @@ const Navbar = () => {
 
   //nav items
   const navItems = [
-    {link: "Home", path:"/"},
-    {link: "About", path:"/about"},
-    {link: "Shop", path:"/shop"},
-    {link: "Sell Your Book", path:"/admin/dashboard"},
-    {link: "Blog", path:"/blog"},
-  ]
+    { link: "Home", path: "/" },
+    { link: "About", path: "/about" },
+    { link: "Shop", path: "/shop" },
+    { link: "Sell Your Book", path: "/admin/dashboard" },
+    { link: "Blog", path: "/blog" },
+  ];
 
   return (
     <header>
-        <nav>
-            <div>
-                {/*logo*/}
-                <Link to="/"><img src="/book.png" className="h-10 w-10"/>Bookish</Link>
-            </div>
-        </nav>
+      <nav>
+        <div>
+          {/*logo*/}
+          <Link
+            to="/"
+            className="text-2xl font-bold text-slate flex items-center gap-2 mx-2 my-2 hover:text-slate-dark"
+          >
+            <img src="/book.png" className="h-10 w-10 inline-block" />
+            Bookish
+          </Link>
+
+          {/*nav items for large device*/}
+          <ul className="md:flex space-x-12 hidden">
+            {navItems.map(({ link, path }) => (
+              <Link
+                key={path}
+                to={path}
+                className="block text-slate text-base cursor-pointer uppercase hover:text-slate-dark"
+              >
+                {link}
+              </Link>
+            ))}
+          </ul>
+
+          {/* btn for lg devices*/ }
+          <div className="space-x-12 hidden lg:flex items-center">
+            <button><FaBars className="w-5 text-slate hover:text-slate-dark"/></button>
+          </div>
+        </div>
+      </nav>
     </header>
-  )
+  );
 };
 
 export default Navbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'slate': '#475569',
+      'slate-dark': '#1e293b',
+      
+    },
+    fontFamily: {
+      sans: ['Playpen Sans', 'cursive'],
+    },
     extend: {},
   },
   plugins: [],
