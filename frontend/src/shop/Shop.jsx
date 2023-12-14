acff4309aa6f0ccd3477410c333cfd9adf16c4f9
Amrita Gupta <amamrita0818@gmail.com>
2023-12-14 23:48:10 +0530

    creating shop section

--- frontend/src/shop/Shop.jsx
+++ frontend/src/shop/Shop.jsx
@@ -1,8 +1,15 @@
-
+import { useEffect, useState } from "react"
 
 const Shop = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:800/all-books").then(res => res.json()).then(data => setBooks(data));
+  }, [] )
   return (
-    <div>Shop</div>
+    <div className="mt-28 px-4 lg:px-32">
+      <h2 className="text-5xl font-bold text-center">All Books are here</h2>
+    </div>
   )
 }
 
