2107105c36c0be64f5ab072762466ca1fb832d13
Amrita Gupta <amamrita0818@gmail.com>
2023-12-27 21:40:32 +0530

    completed manageBooks Component

--- frontend/src/dashboard/ManageBooks.jsx
+++ frontend/src/dashboard/ManageBooks.jsx
@@ -1,6 +1,6 @@
 import { Table } from "flowbite-react";
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 const ManageBooks = () => {
   const [allBooks, setAllBooks] = useState([]);
   useEffect(() => {
@@ -8,6 +8,16 @@ const ManageBooks = () => {
       .then((res) => res.json())
       .then((data) => setAllBooks(data));
   }, []);
+
+  //delete book
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`http://localhost:8000/book/${id}`, {
+      method:"DELETE",
+    }).then(res => res.json()).then(data => {alert("Book is deleted successfully!")
+    // setAllBooks(data);
+  })
+  }
   return (
     <div className="px-4 my-12">
       <h2 className="mb-8 text-3xl font-bold">Manage Your Books</h2>
@@ -32,20 +42,17 @@ const ManageBooks = () => {
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                 {book.title}
               </Table.Cell>
-              <Table.Cell>
-                {book.author}
-              </Table.Cell>
-              <Table.Cell>
-                {book.category}
-              </Table.Cell>
+              <Table.Cell>{book.author}</Table.Cell>
+              <Table.Cell>{book.category}</Table.Cell>
               <Table.Cell>$10.00</Table.Cell>
               <Table.Cell>
-                <a
-                  href="#"
-                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                <Link
+                  to={`/admin/dashboard/edit-books/${book._id}`}
+                  className="font-medium text-blue hover:underline dark:text-blue-dark mr-5"
                 >
                   Edit
-                </a>
+                </Link>
+                <button onClick={() => handleDelete(book._id)} className="bg-blue px-4 py-1 font-semibold text-off-white rounded-sm hover:bg-blue-extra-dark">Delete</button>
               </Table.Cell>
             </Table.Row>
           </Table.Body>
