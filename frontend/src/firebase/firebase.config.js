81ee3c4c52c51277d7a799155f82868b3a7a631c
Amrita Gupta <amamrita0818@gmail.com>
2023-12-28 22:19:06 +0530

    installing firebase ad creating firebase.config.js

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD13budZOdata_0MIL4KUYlTSeOag65mmQ",
+  authDomain: "bookish-e3113.firebaseapp.com",
+  projectId: "bookish-e3113",
+  storageBucket: "bookish-e3113.appspot.com",
+  messagingSenderId: "234507141679",
+  appId: "1:234507141679:web:9d1831b61f6305e7c13cda",
+  measurementId: "G-D6X259EKSZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- frontend/src/home/OtherBooks.jsx
+++ frontend/src/home/OtherBooks.jsx
@@ -5,7 +5,7 @@ const OtherBooks = () => {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:8000/all-books").then(res => res.json()).then(data => setBooks(data.slice(4,10)))
+        fetch("http://localhost:8000/all-books").then(res => res.json()).then(data => setBooks(data.slice(4,12)))
     }, [])
   return (
     <div>
